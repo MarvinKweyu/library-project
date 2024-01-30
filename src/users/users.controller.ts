01a5fb0ec2e010320c9df0ba6fd20ef7be9fb152
Marvin Kweyu <marvin@helloduty.com>
2024-01-30 16:07:03 +0300

    feat: update user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') //  /users
+export class UsersController {
+  @Get() // users or users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    console.log(role);
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') //patch /users/:id
+  update(@Param('id') id: string, @Body() updatedUser: object) {
+    return { id, ...updatedUser };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
